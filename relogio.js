b96567bdeda27235e2c9934e84b85701da88547c
Renato dos Santos <santos.renato@hotmail.com>
2022-02-26 20:42:55 -0300

    adicionando relogio

--- relogio.js
+++ relogio.js
@@ -1,3 +1,13 @@
+/*
+     dias da semana status 
+     sabado status
+    domingo status
+
+    "frase de boas vinadas com dia e o status"
+
+*/
+
+
 const relogio = document.querySelector('.relogio')
 const funcionamento = document.querySelector('.funcionamento')
 
@@ -7,21 +17,52 @@ const calendario = () => {
     let data = new Date()
     let hora = data.getHours()
     let diaSem = data.getDay()
-
-
-    if(diaSem >= 1 && diaSem <= 5){
-        if (hora > 8 && hora < 18) {
-            estado = 'Abertos'
+    // Domingo
+    if (diaSem == 0) {
+        estado = 'Fechados tenha um bom fim de semana.'
+        // Dias da semana 
+    } else if (diaSem >= 1 && diaSem <= 5) {
+        if (hora >= 8 && hora <20) {
+            estado = 'Aberto fique a vontade para nos fazer uma visita.'
         } else {
-            estado = 'Fechados'
+            estado = 'no momento estamos fechados.'
         }
-    }else{
-        estado = `Fechado Um bom fim de semana`
+        // Sabado
+    } else if (diaSem == 6) {
+        if (hora >= 8 && hora <18) {
+            estado = 'Aberto fique a vontade para nos fazer uma visita.'
+        } else {
+            estado = 'Fechados tenha um bom fim de semana.'
+        }
     }
-
-
-    funcionamento.innerHTML += `Estamos ${estado}`
     
+    switch (diaSem){
+        case 0 :
+            dia = ('Domingo')
+            break
+        case 1 :
+            dia = ('Segunda-Feira')
+            break
+        case 2 :
+            dia = ('Terça-Feira')
+            break
+        case 3 :
+            dia = ('Quarta-Feira')
+            break
+        case 4 :
+            dia = ('Quinta-Feira')
+            break
+        case 5 :
+            dia = ('Sexta-Feira')
+            break
+        case 6 :
+            dia = ('Sabado')
+            break
+        default:
+            dia = ('[ERRO]')
+            
+    }
+    funcionamento.innerHTML += `Hoje é ${dia} estamos ${estado}`
 }
 
 const getClockHTML = (hours, minutes, seconds) =>
